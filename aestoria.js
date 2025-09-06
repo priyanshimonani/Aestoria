7516b486a91ed2dc43eb33b601903331fa039ad0
priyanshimonani <priyanshimonani@gmail.com>
2025-09-06 17:08:16 +0530

    the commit before everything went down

--- aestoria.js
+++ aestoria.js
@@ -1,7 +1,7 @@
 function search(category) {
   document.getElementById("trial").innerText = category;
   document.getElementById("trial").innerText = "yayy";
-  fetch(`https://api.harvardartmuseums.org/object?apikey=a3516175-d7de-4012-9a7c-73df61ff62c5&size=50&classification=${category}`)
+  fetch(`https://api.harvardartmuseums.org/object?apikey=a3516175-d7de-4012-9a7c-73df61ff62c5&size=20&classification=${category}`)
     .then(response => response.json())
     .then(data => {
       const container = document.getElementById("images");
@@ -11,12 +11,15 @@ function search(category) {
         data.records.forEach(item => {
           if (item.primaryimageurl) {
             const image = document.createElement("div");
+            const artist = item.people && item.people.length > 0 ? item.people[0].name : "Unknown Artist";
             image.innerHTML = `
-              <div style="background:white; padding:10px; margin:15px; border-radius:8px; 
-              box-shadow: 0 4px 8px rgba(0,0,0,0.2); text-align:center; width:320px;">
-              <img src="${item.primaryimageurl}" width="300" style="margin:10px; border:1px solid #ccc"/>
+              <div style="background:white; color:black; padding:10px; margin:15px; border-radius:8px; padding-top:4px; 
+              box-shadow: 0 4px 8px rgba(0,0,0,0.2); text-align:center; width:320px; ">
+              <img src="${item.primaryimageurl}" loading="lazy" width="300" style="margin:auto; border:1px solid #ccc; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.2); max-height: 500px;"/>
               <h3>${item.title}</h3>
-              <h3>${item.dated}</h3>
+              <h3>${artist}</h3>
+              <h3>${item.description}</h3>
+              
               </div>
             `;
             container.appendChild(image);
